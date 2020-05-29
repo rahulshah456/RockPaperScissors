a015481fff67cd9d7455a91d7f808e2020f2e0b3
Rahul Shah <rahulkumarshah5000@gmail.com>
2020-05-30 03:54:39 +0530

    initial commit

---
+++ app/src/main/java/com/example/retailpulseassignment/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.retailpulseassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
